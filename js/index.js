11a6719e10c48367fb1a52d57663318276de26cf
FloKots <florencekotohoyoh@gmail.com>
2022-03-04 15:32:21 +0300

    add index.html initial page structure

---
+++ js/index.js
@@ -0,0 +1,24 @@
+//Data types, Array, JavaScript functions
+//Control flow, Logical operators
+
+//Takes a user's birthday
+//Validate the entered date - both month and day
+//February has 28/29 days, others 31 days. 
+//Calculates the day of the week 
+//Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+//  where;
+
+//  CC - is the century digits. For example 1989 has CC = 19
+
+//  YY - is the Year digits (1989 has YY = 89)
+
+//  MM -  is the Month
+
+//  DD - is the Day of the month 
+
+//  mod - is the modulus function ( % )
+//Choose gender
+//Submit 
+//Output Akan Name
+
