f292b3c41b1e99c77f329cf1d74d41b1c2d8ce18
FloKots <florencekotohoyoh@gmail.com>
2022-03-07 06:45:35 +0300

    add getDay function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
+var getAkanName = function () {
+  var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var date = document.getElementById("date").value;
+  var birthDate = new Date(date);
+	var day = birthDate.getDay();
+  
+}
