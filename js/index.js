b973887009991cbd9243ad9f71385cae1676e57d
FloKots <florencekotohoyoh@gmail.com>
2022-03-07 10:50:14 +0300

    add getAkanName function

--- js/index.js
+++ js/index.js
@@ -4,6 +4,45 @@ var getAkanName = function () {
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   var date = document.getElementById("date").value;
   var birthDate = new Date(date);
-	var day = birthDate.getDay();
-  
+  var day = birthDate.getDay();
+  var gender = document.getElementsByName("gender");
+
+  for (var i = 0; i < gender.length; i++) {
+    if (gender[i].checked && gender[i].value === 'male'){
+      let maleResponse = "You were born on a " + weekDays[day] + " Your Akan name is " + maleNames[day];
+      document.getElementById("response").innerHTML = maleResponse;
+      break;
+    } else if (gender[i].checked && gender[i].value === 'female') {
+      let femaleResponse = "You were born on a " + weekDays[day] + " Your Akan name is " + femaleNames[day];
+      document.getElementById("response").innerHTML = femaleResponse;
+      break;
+    } else
+    {
+      document.getElementById("response").innerHTML = "Please select your gender!";
+    }
+  }
+  document.getElementById("form").addEventListener("click", function(event){
+    event.preventDefault()
+  });
 }
+
+// function clearForm() {
+//   document.getElementById("form").reset();
+// }
+
+
+// document.getElementById("form").reset();
+// if (gender[i].checked) {
+//   if (gender[i].value === 'male'){
+//     let maleResponse = "You were born on a " + weekDays[day] + " Your Akan name is " + maleNames[day];
+//     document.getElementById("response").innerHTML = maleResponse;
+//     console.log(maleResponse);
+//   }else{
+//     let femaleResponse = "You were born on a " + weekDays[day] + " Your Akan name is " + femaleNames[day];
+//     document.getElementById("response").innerHTML = femaleResponse;
+//     console.log(femaleResponse);
+//   }
+// }else {
+//   alert("Please select your gender!")
+// }
+// }
