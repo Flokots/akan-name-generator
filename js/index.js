46b33c793002a3a16ecffe021e9ce5b9014061e0
FloKots <florencekotohoyoh@gmail.com>
2022-03-07 14:51:21 +0300

    style response div

--- js/index.js
+++ js/index.js
@@ -13,11 +13,11 @@ var getAkanName = function () {
   } else {
     for (var i = 0; i < gender.length; i++) {
       if (gender[i].checked && gender[i].value === 'male') {
-        let maleResponse = "You were born on a " + weekDays[day] + " Your Akan name is " + maleNames[day];
+        let maleResponse = "You were born on a " + weekDays[day] + "."+ " Your Akan name is " + maleNames[day] + ".";
         document.getElementById("response").innerHTML = maleResponse;
         break;
       } else if (gender[i].checked && gender[i].value === 'female') {
-        let femaleResponse = "You were born on a " + weekDays[day] + " Your Akan name is " + femaleNames[day];
+        let femaleResponse = "You were born on a " + weekDays[day] + "." + " Your Akan name is " + femaleNames[day] +".";
         document.getElementById("response").innerHTML = femaleResponse;
         break;
       } else {
