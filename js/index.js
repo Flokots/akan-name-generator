0e4ff55bac91414db9bb3dca92fb0f46891fd551
FloKots <florencekotohoyoh@gmail.com>
2022-03-07 14:53:04 +0300

    fix typo in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var getAkanName = function () {
-  var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   var date = document.getElementById("date").value;
@@ -13,11 +13,11 @@ var getAkanName = function () {
   } else {
     for (var i = 0; i < gender.length; i++) {
       if (gender[i].checked && gender[i].value === 'male') {
-        let maleResponse = "You were born on a " + weekDays[day] + "."+ " Your Akan name is " + maleNames[day] + ".";
+        let maleResponse = "You were born on a " + weekDays[day] + "." + " Your Akan name is " + maleNames[day] + ".";
         document.getElementById("response").innerHTML = maleResponse;
         break;
       } else if (gender[i].checked && gender[i].value === 'female') {
-        let femaleResponse = "You were born on a " + weekDays[day] + "." + " Your Akan name is " + femaleNames[day] +".";
+        let femaleResponse = "You were born on a " + weekDays[day] + "." + " Your Akan name is " + femaleNames[day] + ".";
         document.getElementById("response").innerHTML = femaleResponse;
         break;
       } else {
